a2099d8cc845a592a002f97107236b29409f002a
pablo <pablosouto6@gmail.com>
2021-12-23 12:19:12 +0100

    Skate Model

--- server/models/skates.js
+++ server/models/skates.js
@@ -1,15 +1,14 @@
 const mongoose = require(`mongoose`)
 
-const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-const passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
+const types = ['deck', 'truck', 'wheels']
 
 let userSchema = new mongoose.Schema(
    {
-        name: {type: String},
-        email: {type: String, match: [emailPattern, "Email must be valid."]},
-        password: {type: String, /* match: [passwordPattern, "Password must have at leat 6 characters, including 1 number and 1 special character."] */},
-        confirmPassword: {type: String, /* match: [this.password, "Passwords must match."] */}
+        type: {type: String, match:[types, "Invalid product type"]},
+        size: {type: Number},
+        brand: {type: String},
+        price: {type: Number, validate: function(){return this.price > 0}}
    },
    {
         collection: `Skates`
--- server/models/users.js
+++ server/models/users.js
@@ -2,15 +2,13 @@ const mongoose = require(`mongoose`)
 
 const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-const passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
-
 const accessLevels = [0, 1, 2]
 
 let userSchema = new mongoose.Schema(
    {
         name: {type: String},
         email: {type: String, match: [emailPattern, "Email must be valid."]},
-        password: {type: String, /* match: [passwordPattern, "Password must have at leat 6 characters, including 1 number and 1 special character."] */},
+        password: {type: String},
         accessLevel: {type: Number, match: [accessLevels, "Invalid Access Level"]}
    },
    {
