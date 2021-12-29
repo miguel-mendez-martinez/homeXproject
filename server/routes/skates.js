a24c5a13cb98ecccd31f0ae1eabe54960ed373a5
pablo <pablosouto6@gmail.com>
2021-12-29 12:57:39 +0100

    skates middleware

--- server/routes/skates.js
+++ server/routes/skates.js
@@ -5,6 +5,92 @@ const jwt = require('jsonwebtoken')
 const multer  = require('multer')
 var upload = multer({dest: `${process.env.UPLOADED_FILES_FOLDER}`})
 
+const checkUserLogged = (req, res, next) =>
+{
+    jwt.verify(req.headers.authorization, JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) => 
+    {
+        if (err) 
+        { 
+            return res.json({errorMessage:`User is not logged in`})
+        }
+        else 
+        {
+            req.decodedToken = decodedToken
+            return next()
+        }
+    })
+}
+
+
+const checkIfAdmin = (req, res, next) =>
+{
+    if(req.decodedToken.accessLevel >= process.env.ACCESS_LEVEL_ADMIN)
+    {    
+        return next()
+    }
+    else
+    {
+        return res.json({errorMessage:`User is not an administrator`})
+    }
+}
+
+const findById = (req, res, next) =>{
+    carsModel.findById(req.params.id, (error, data) => 
+    {
+        if(error)
+            res.json({errorMessage: `Product not found`})
+        else
+            res.json(data)
+    })
+}
+
+const addProduct = (req, res, next) =>{
+    let productDetails = new Object()
+    productDetails.type = req.body.type
+    productDetails.size = req.body.size
+    productDetails.brand = req.body.brand
+    productDetails.price = req.body.price
+    productDetails.photos = []
+
+    req.files.map((file, index) =>
+    {
+        productDetails.photos[index] = {filename:`${file.filename}`}
+    })
+    
+    skatesModel.create(productDetails, (error, data) =>
+    {
+        if(error){
+            res.json({errorMessage: `${error}`})
+            //res.json({errorMessage:`Bad Request`})
+        }else{
+            res.json(data)
+        }
+        
+    })
+}
+
+const deleteProduct = (req, res, next) =>{
+    skatesModel.findByIdAndRemove(req.params.id, (error, data) => 
+    {
+        if(error){
+            res.json({errorMessage: `${error}`})
+        }else{
+            res.json(data)
+        }
+    })
+}
+
+const updateProduct = (req, res, next) =>{
+    skatesModel.findByIdAndUpdate(req.params.id, {$set: req.body}, (error, data) => 
+    {
+        if(error){
+            console.log(error)
+        }else{
+            res.json(data)
+        }
+    })
+}
+
 router.get('/DisplayAllSkates', (req, res) => 
 {
     skatesModel.find({}, (error, data) =>
@@ -25,25 +111,7 @@ router.get('/DisplayAllSkates/:category', (req, res) =>
     })
 })
 
-router.get(`/DisplayAllSkates/:id`, (req, res) => {
-    jwt.verify(req.headers.authorization, JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) => 
-    {
-        if (err) 
-        { 
-            res.json({errorMessage:`User is not logged in`})
-        }
-        else
-        {
-            carsModel.findById(req.params.id, (error, data) => 
-            {
-                if(error)
-                    res.json({errorMessage: `Product not found`})
-                else
-                    res.json(data)
-            })
-        }
-    })
-})
+router.get(`/DisplayAllSkates/:id`, checkUserLogged, findById)
 
 //Return a photo
 router.get(`/DisplayAllSkates/photo/:filename`, (req, res) => 
@@ -61,65 +129,10 @@ router.get(`/DisplayAllSkates/photo/:filename`, (req, res) =>
     })             
 })
 
+router.post('/DisplayAllSkates', upload.array("productPhotos", parseInt(process.env.MAX_NUMBER_OF_UPLOAD_FILES_ALLOWED)), checkUserLogged, checkIfAdmin, addProduct)
 
-router.post('/DisplayAllSkates', upload.array("productPhotos", parseInt(process.env.MAX_NUMBER_OF_UPLOAD_FILES_ALLOWED)), (req, res) => 
-{
-    console.log()
-    jwt.verify(req.headers.authorization, process.env.JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) =>{
-        if(err){
-            res.json({errorMessage: `${err}`})
-        }else{
-            let productDetails = new Object()
-            productDetails.type = req.body.type
-            productDetails.size = req.body.size
-            productDetails.brand = req.body.brand
-            productDetails.price = req.body.price
-            productDetails.photos = []
+router.delete('/DisplayAllSkates/:id', checkUserLogged, checkIfAdmin, deleteProduct)
 
-            req.files.map((file, index) =>
-            {
-                productDetails.photos[index] = {filename:`${file.filename}`}
-            })
-            
-            skatesModel.create(productDetails, (error, data) =>
-            {
-                if(error){
-                    res.json({errorMessage: `${error}`})
-                    //res.json({errorMessage:`Bad Request`})
-                }else{
-                    res.json(data)
-                }
-                
-            })
-        }
-        
-    })
-    
-})
-
-router.delete('/DisplayAllSkates/:id', (req, res) => 
-{
-    skatesModel.findByIdAndRemove(req.params.id, (error, data) => 
-    {
-        if(error){
-            console.log(error)
-        }else{
-            res.json(data)
-        }
-    })
-})
-
-router.put(`/DisplayAllSkates/:id`, (req, res) => 
-{
-    skatesModel.findByIdAndUpdate(req.params.id, {$set: req.body}, (error, data) => 
-    {
-        if(error){
-            console.log(error)
-        }else{
-            res.json(data)
-        }
-    })
-    
-})
+router.put(`/DisplayAllSkates/:id`, checkUserLogged, checkIfAdmin, updateProduct)
 
 module.exports = router
