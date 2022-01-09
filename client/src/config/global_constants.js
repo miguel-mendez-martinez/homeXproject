313cb5e2430a5d80996e781e431fa9f86483873d
Miguel Méndez Martínez <miguetripleM@gmail.com>
2022-01-09 17:45:37 +0100

    nueva tarjet ade credito de la mama

--- client/src/config/global_constants.js
+++ client/src/config/global_constants.js
@@ -7,3 +7,15 @@ export const ACCESS_LEVEL_NORMAL_USER = 1
 export const ACCESS_LEVEL_ADMIN = 2
 
 export const SANDBOX_CLIENT_ID = "AReXuG8eF2xUoUsEw76IW4groDOqYRTgl5408h0n7sYTaVhCGDDdUIHFxjDCiBFwhmlD7facD9GfCSTo"
+
+//credit card details
+
+/* 
+    Card Type: Visa
+
+    Card Number: 4063339830992366
+
+    Expiration Date: 02/2027
+
+    CVV: 985
+*/
