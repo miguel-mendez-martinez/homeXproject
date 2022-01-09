9c302f7808bf72ec25e9c4ed0a59d7cd80fa8dfb
Miguel Méndez Martínez <miguetripleM@gmail.com>
2022-01-09 17:44:27 +0100

    cosas que no se porque se ha commiteado

--- client/src/config/global_constants.js
+++ client/src/config/global_constants.js
@@ -5,3 +5,5 @@ export const SERVER_HOST = `http://localhost:4000`
 export const ACCESS_LEVEL_GUEST = 0
 export const ACCESS_LEVEL_NORMAL_USER = 1
 export const ACCESS_LEVEL_ADMIN = 2
+
+export const SANDBOX_CLIENT_ID = "AReXuG8eF2xUoUsEw76IW4groDOqYRTgl5408h0n7sYTaVhCGDDdUIHFxjDCiBFwhmlD7facD9GfCSTo"
