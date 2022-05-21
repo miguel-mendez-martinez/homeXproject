49bda3963715072c8b264e699ee9d23d643c574f
pablo <pablosouto6@gmail.com>
2022-05-21 17:41:30 +0200

    properties route

--- server/routes/properties.js
+++ server/routes/properties.js
@@ -1,9 +1,147 @@
 const router = require(`express`).Router()
 const propertiesModel = require(`../models/property`)
+const contractsModel = require(`../models/contracts`)
 const createError = require('http-errors')
 
 //Middleware
 
+const checkUserLogged = (req, res, next) =>
+{
+    jwt.verify(req.headers.authorization, process.env.JWT_PRIVATE_KEY, {algorithm: "HS256"}, (err, decodedToken) => 
+    {
+        if (err) 
+        { 
+            return next(createError(400, "User is not logged in."))
+        }
+        else 
+        {
+            req.decodedToken = decodedToken
+            return next()
+        }
+    })
+}
+
+const checkPropertyDontExists = (req, res, next) =>
+{
+    propertiesModel.findOne({address}, (error, data) =>
+    {
+        if(error){
+            return next(createError(400, "Property already exists"))
+        }else
+            return next()  
+    })
+}
+
+const addProperty = (req, res, next) =>
+{
+    let property = new Object()
+    property.tenant = req.body.tenant
+    property.address = req.body.address
+    property.area = req.body.area
+    property.price = req.body.price
+    property.residents = 'none'
+    property.images = []
+
+    req.files.map((file, index) =>
+    {
+        property.images[index] = {filename:`${file.filename}`}
+    })
+
+    propertiesModel.create(property, (error, data) =>
+    {
+        if(error){
+            return next(createError(400, `Error on property creation.`))
+        }else{
+            res.json(data)
+        }
+        
+    })
+}
+
+const updateProperty = (req, res, next) =>{ //At the moment is not possible to update images
+
+    propertiesModel.findByIdAndUpdate(req.params.id, {$set: req.body}, (error, data) => 
+    {
+        if(error){
+            return next(createError(400, `Error on property update.`))
+        }else{
+            res.json(data)
+        }
+    })
+}
+
+const deleteProperty = (req, res, next) =>{
+    let pathArray = __dirname.split('\\')
+    let path = pathArray.splice(-0, pathArray.length - 1).join('\\')
+
+    propertyModel.findByIdAndRemove(req.params.id, (error, data) => 
+    {
+        if(error){
+            return next(createError(400, `Error on property delete.`))
+        }else{
+            fs.unlink(`${path}\\uploads\\${data.images[0].filename}`, (err) => {
+                if(err)
+                    return next(createError(400, `Error on image deleting.`))
+                else
+                    res.json(data)
+            })
+        }
+    })
+}
+
+const checkAvaliable = (req, res, next) =>{
+
+    propertiesModel.findOne(req.params.id, (error, data) =>
+    {
+        if(error){
+            return next(createError(400, "Error checking avaliability"))
+        }else{
+            if(data){
+                if(data.residents == 'none')
+                    return next()
+                else
+                return next(createError(400, "Property is not avaliable"))
+            }
+        }
+              
+    })
+}
+
+const rentProperty = (req, res, next) =>{
+
+    propertiesModel.findByIdAndUpdate(req.params.id, {"residents": req.body.residents}, (error, data) => 
+    {
+        if(error){
+            return next(createError(400, `Error on property renting.`))
+        }else{
+            return next()
+        }
+    })
+}
+
+const generateContract = (req, res, next) =>{
+
+    let contract = new Object()
+    contract.date = req.body.date
+    contract.tenant = req.body.tenant
+    contract.residents = req.body.residents
+    contract.property = req.body.property
+    contract.expireDate = req.body.expireDate
+    contract.moneyAmount = req.body.moneyAmount
+    contract.monthlyDeadLine = req.body.monthlyDeadLine
+    
+    contractsModel.create(contract, (error, data) =>
+    {
+        if(error){
+            return next(createError(400, `Error on contract creation.`))
+        }else{
+            res.json(data)
+        }       
+    })
+}
+
+
+//routes
 router.get('/Properties', (req, res) => 
 {
     propertiesModel.find({}, (error, data) =>
@@ -13,4 +151,13 @@ router.get('/Properties', (req, res) =>
         }
     })
 })
+
+router.post('/Properties/AddNew', upload.array("propertyImages", parseInt(process.env.MAX_NUMBER_OF_UPLOAD_FILES_ALLOWED)),checkUserLogged, checkPropertyDontExists, addProperty)
+
+router.put('/Properties/:id', checkUserLogged, updateProperty)
+
+router.delete('/Properties/:id', checkUserLogged, deleteProperty)
+
+router.post('/Properties/rentProperty/:id/:residents', checkUserLogged, checkAvaliable, rentProperty, generateContract)
+
 module.exports = router
